39b2982e091e739b9a984db255ee8ff59dc0072e
olegpl1993 <olegpl1993@gmail.com>
2023-06-13 21:34:59 +0300

    feat: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { writeFile, access, constants } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const create = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url); // путь к текущему файлу
+  const __dirname = dirname(__filename); // путь к текущей папке
+  const pathToFiles = join(__dirname, "files"); // путь к целевой папке
+  const pathToNewFile = join(pathToFiles, "fresh.txt"); // путь к создаваемому файлу
+  // проверка существования файла
+  access(pathToNewFile, constants.F_OK, (error) => {
+    if (error) {
+      // создает файл c текстом
+      writeFile(pathToNewFile, "I am fresh and young", (err) => {});
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
