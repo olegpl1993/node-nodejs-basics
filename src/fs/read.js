4de584b941271605b169c64d9840aa97d05130ba
olegpl1993 <olegpl1993@gmail.com>
2023-06-13 23:44:39 +0300

    feat: read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { access, constants, readFile } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url); // путь к текущему файлу
+  const __dirname = dirname(__filename); // путь к текущей папке
+  const pathToFiles = join(__dirname, "files"); // путь к целевой папке
+  const pathToFile = join(pathToFiles, "fileToRead.txt"); // путь к целевому файлу
+  // проверка существования файла
+  access(pathToFile, constants.F_OK, (error) => {
+    if (error) {
+      throw new Error("FS operation failed");
+    } else {
+      // чтение файла и вывод содержимого в консоль
+      readFile(pathToFile, "utf8", (err, data) => {
+        console.log(data);
+      });
+    }
+  });
 };
 
 await read();
