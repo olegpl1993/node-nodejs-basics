fd717bd21d12cd37227f996e7734e4997cdbcbb7
olegpl1993 <olegpl1993@gmail.com>
2023-06-13 23:34:25 +0300

    feat: list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import { access, constants, readdir } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url); // путь к текущему файлу
+  const __dirname = dirname(__filename); // путь к текущей папке
+  const pathToFiles = join(__dirname, "files"); // путь к целевой папке
+  // проверка существования папки с файлами
+  access(pathToFiles, constants.F_OK, (error) => {
+    if (error) {
+      throw new Error("FS operation failed");
+    } else {
+      // получает массив файлов вложенных в папку
+      readdir(pathToFiles, { withFileTypes: true }, (err, files) => {
+        // перебор массива по обьектам
+        files.forEach((file) => {
+          console.log(file.name);
+        });
+      });
+    }
+  });
 };
 
 await list();
