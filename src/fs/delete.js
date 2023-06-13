9a1c2bde317b32d39a142b7a0048f2d6b1189a30
olegpl1993 <olegpl1993@gmail.com>
2023-06-13 23:21:52 +0300

    feat: delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { access, constants, unlink } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url); // путь к текущему файлу
+  const __dirname = dirname(__filename); // путь к текущей папке
+  const pathToFiles = join(__dirname, "files"); // путь к целевой папке
+  const pathToFile = join(pathToFiles, "fileToRemove.txt"); // путь к целевому файлу
+  // проверка существования файла
+  access(pathToFile, constants.F_OK, (error) => {
+    if (error) {
+      throw new Error("FS operation failed");
+    } else {
+      // удаление файла
+      unlink(pathToFile, (err) => {});
+    }
+  });
 };
 
 await remove();
