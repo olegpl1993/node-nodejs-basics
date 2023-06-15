dcd3a441ecf0946a687f1c925accd6feb6acfe30
olegpl1993 <olegpl1993@gmail.com>
2023-06-15 23:38:30 +0300

    feat: calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from "crypto";
+import { readFile } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url); // путь к текущему файлу
+  const __dirname = dirname(__filename); // путь к текущей папке
+  const pathToFiles = join(__dirname, "files"); // путь к целевой папке
+  const pathToFile = join(pathToFiles, "fileToCalculateHashFor.txt"); // путь к целевому файлу
+  // чтение файла
+  readFile(pathToFile, "utf8", (err, data) => {
+    const hash = createHash("sha256").update(data).digest("hex"); // вычисление хэша SHA256
+    console.log(hash);
+  });
 };
 
 await calculateHash();
