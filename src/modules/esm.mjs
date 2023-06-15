28b633b825455532da47e965f58be126ee07f5ef
olegpl1993 <olegpl1993@gmail.com>
2023-06-15 22:38:10 +0300

    feat: esm

---
+++ src/modules/esm.mjs
@@ -0,0 +1,45 @@
+import { sep } from "path";
+import { release, version } from "os";
+import { createServer as createServerHttp } from "http";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import a from "./files/a.json" assert { type: "json" };
+import b from "./files/b.json" assert { type: "json" };
+import "./files/c.js";
+
+const random = Math.random();
+
+let unknownObject;
+const __filename = fileURLToPath(import.meta.url); // путь к текущему файлу
+const __dirname = dirname(__filename); // путь к текущей папке
+
+if (random > 0.5) {
+  unknownObject = a;
+} else {
+  unknownObject = b;
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end("Request accepted");
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
+});
+
+export default {
+  unknownObject,
+  myServer,
+};
