556ed9aa46385a2e7d9378e6f063e6195d032dd6
olegpl1993 <olegpl1993@gmail.com>
2023-06-17 00:50:50 +0300

    feat: write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url); // путь к текущему файлу
+  const __dirname = dirname(__filename); // путь к текущей папке
+  const pathToFiles = join(__dirname, "files"); // путь к целевой папке
+  const pathToFile = join(pathToFiles, "fileToWrite.txt"); // путь к целевому файлу
+
+  const writableStream = createWriteStream(pathToFile, { flags: "a" }); // создание потока записи в файл
+  process.stdin.setEncoding("utf8"); // установка кодировки ввода
+  // слушает ввод в консоль
+  process.stdin.on("data", (chunk) => {
+    writableStream.write(chunk); // передает информацию из ввода в поток записи
+  });
 };
 
 await write();
