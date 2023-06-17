e68391ae58b2eb011c4df289b5933a53b6113e04
olegpl1993 <olegpl1993@gmail.com>
2023-06-17 17:52:08 +0300

    feat: trnsform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  // создание потока преобразования
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reverseData = chunk.toString().split('').reverse().join(''); // переворачиваем строку
+      process.stdout.write(reverseData); // Выводим результат в консоль
+      callback(); // завершение обработки текущего фрагмента данных
+    },
+  });
+  process.stdin.pipe(transformStream);
 };
 
 await transform();
