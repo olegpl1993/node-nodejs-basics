d79af87c0a01ae957363f6054f8b6ab5a743e872
olegpl1993 <olegpl1993@gmail.com>
2023-06-17 22:19:24 +0300

    feat: decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createUnzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url); // путь к текущему файлу
+  const __dirname = dirname(__filename); // путь к текущей папке
+  const pathToFiles = join(__dirname, "files"); // путь к целевой папке
+  const pathToFile = join(pathToFiles, "archive.gz"); // путь к целевому файлу
+  const pathToZipFile = join(pathToFiles, "fileToCompress.txt"); // путь к разжатому файлу
+
+  const readStream = createReadStream(pathToFile); // создание потока чтения
+  const writeStream = createWriteStream(pathToZipFile); // создание потока записи
+  const unzip = createUnzip(); // создание потока разжатия
+  readStream.pipe(unzip).pipe(writeStream); // читает файл, передает в поток разжатия, передает в поток записи
 };
 
 await decompress();
