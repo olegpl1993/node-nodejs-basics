0a63eaf8e4cc25629ec5f025e8c1a306db25ce35
olegpl1993 <olegpl1993@gmail.com>
2023-06-18 16:39:39 +0300

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const performCalculations = async () => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url); // путь к текущему файлу
+  const __dirname = dirname(__filename); // путь к текущей папке
+  const pathToFile = join(__dirname, "worker.js"); // путь к целевому файлу
+  const numThreads = cpus().length; // кол-во ядер процессора
+
+  const workerPromises = [];
+  for (let i = 0, j = 10; i < numThreads; i++, j++) {
+    const worker = new Worker(pathToFile); // создание рабочего потока
+    worker.postMessage(j);
+    const workerPromise = new Promise((resolve) => {
+      console.log(i, " = ", j);
+      worker.on("message", () => {
+        console.log("worker.on");
+        resolve;
+      });
+    });
+    workerPromises.push(workerPromise);
+  }
+  const results = await Promise.all(workerPromises);
+  console.log("results", results);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,34 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { Transform } from "stream";
+import { parentPort } from "worker_threads";
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  console.log("sendResult");
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const value = nthFibonacci(Number(chunk.toString()));
+      callback(null, value.toString());
+      console.log("transform");
+    },
+  });
+  // Обработчик события 'message' для чтения данных из основного потока
+  parentPort.on("message", (message) => {
+    // Помещаем данные в поток преобразования
+    console.log("parentPort.on message", message);
+    transformStream.write(message.toString());
+  });
+  parentPort.on("close", () => {
+    console.log("parentPort.on close");
+    transformStream.end();
+  });
+  // Обработчик события 'finish' для отправки результатов в основной поток
+  transformStream.on("finish", () => {
+    console.log("transformStream.on finish");
+    const transformedData = transformStream.read();
+    parentPort.postMessage(transformedData);
+  });
 };
 
 sendResult();
