074234727a5abc3549e1e5f9acb0889d64e89080
olegpl1993 <olegpl1993@gmail.com>
2023-06-20 00:00:43 +0300

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { fork } from "child_process";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url); // путь к текущему файлу
+  const __dirname = dirname(__filename); // путь к текущей папке
+  const pathToFiles = join(__dirname, "files"); // путь к целевой папке
+  const pathToFile = join(pathToFiles, "script.js"); // путь к целевому файлу
+  fork(pathToFile, args); // создание дочернего процесса
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2", "someArgument3"]);
